48b00bb57eb26d9f8ff2198acfca5280964b1852
adnen rebai <adnen.Rebai@gmail.com>
2022-09-24 00:20:30 +0100

    update : config date for all soon demo

--- soon-demo/soon-four-four/config.js
+++ soon-demo/soon-four-four/config.js
@@ -1,107 +1,77 @@
-'use strict';
+"use strict";
 
+var background = {
+  // 'gradient' , 'image' , 'slideShow'
+  type: "image",
 
+  //'bg-color-one','bg-color-two','bg-color-three','bg-color-four'
 
-var background ={
-                 // 'gradient' , 'image' , 'slideShow'
-       type   : 'image',
+  gradient: "bg-color-one",
 
-         //'bg-color-one','bg-color-two','bg-color-three','bg-color-four'
-      
-       gradient  : 'bg-color-one',
+  // choose ture to activate background image with the url image_url
 
+  //  or let it to false to have gradient color
+  slideShow: {
+    slides: [
+      {
+        src: "https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-3.jpeg",
+      },
+      {
+        src: "https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-1.jpeg",
+      },
+      {
+        src: "https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-2.jpeg",
+      },
+      {
+        src: "https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-4.jpegb",
+      },
+      {
+        src: "https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-5.jpeg",
+      },
+      {
+        src: "https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-6.jpeg",
+      },
+    ],
+    overlay: "assets/css/overlays/06.png",
+  },
 
-        // choose ture to activate background image with the url image_url 
-       
-        //  or let it to false to have gradient color
-       slideShow :{ 
+  image: {
+    image_url:
+      "https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-6.jpeg",
+  },
 
-             slides    :
-
-             [
-              { src: 'https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-3.jpeg' },
-                { src: 'https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-1.jpeg' },
-                { src: 'https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-2.jpeg' },
-                { src: 'https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-4.jpegb' },
-                { src: 'https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-5.jpeg' },
-                { src: 'https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-6.jpeg' }
-              ],
-                overlay:'assets/css/overlays/06.png'
-
-             },
-
-        image : {
-
-          image_url : "https://raw.githubusercontent.com/adnenre/adnenre.github.io/master/soon-demo/img/img-6.jpeg"
-
-        },
-       
-      
-       particule : true
-
-      } ;
-
-
-
+  particule: true,
+};
 
 /**************************
- **		countDwon      **   
+ **		countDwon      **
  **************************/
 
 var counter = {
-        
+  // "counter-one", "counter-two","counter-three","counter-four"
 
-        // "counter-one", "counter-two","counter-three","counter-four"
+  style: "counter-four",
 
-		style : 'counter-four',
+  launchDate: {
+    // OPTIONS:"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    Month: "May",
 
+    //  DAY : INTEGER[ 1 - 31 ]
 
-		
-    launchDate : {
+    Day: 25,
 
+    // SET YEAR : INTEGER
 
-          // OPTIONS:"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-          Month: 'May',
-
-
-          //  DAY : INTEGER[ 1 - 31 ]
-
-          Day: 25,
-
-           // SET YEAR : INTEGER
-
-          Year: 2022
-    }
-		
-	};
+    Year: 2023,
+  },
+};
 
 /**************************
- **		Ajax Chimp       **   
+ **		Ajax Chimp       **
  **************************/
 
 $("#subscribe_form").ajaxChimp({
+  // Replace your mailchimp post url inside double quote "".
 
-
-	// Replace your mailchimp post url inside double quote "".
-    
-    url: "//novisdev.us15.list-manage.com/subscribe/post?u=202b79afea96f1d57561896f5&amp;id=02ba748be1"    
- 
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  url: "//novisdev.us15.list-manage.com/subscribe/post?u=202b79afea96f1d57561896f5&amp;id=02ba748be1",
+});
