cb3aba2f066e9434534c0ce3e08a10468ddfc24b
Adnen Rebai <adnen.rebai@gmail.com>
2025-07-23 19:04:41 +0200

    fix : use single source for socialLInks

---
+++ src/lib/socialLinks.ts
@@ -0,0 +1,28 @@
+import UpworkIcon from "@/components/ui/UpworkIcon";
+import { Github, Linkedin, Twitter } from "lucide-react";
+export const socialLinks = [
+  {
+    icon: Github,
+    label: "GitHub",
+    href: "https://github.com/adnenre",
+    username: "@adnenre",
+  },
+  {
+    icon: UpworkIcon,
+    label: "Upwork",
+    href: "https://www.upwork.com/freelancers/~01b0151dd79b92ec02",
+    username: "@Adnen R.",
+  },
+  {
+    icon: Linkedin,
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/adnen-r-65843263/",
+    username: "in/adnen",
+  },
+  {
+    icon: Twitter,
+    label: "Twitter",
+    href: "https://twitter.com",
+    username: "@adnen_rebai",
+  },
+];
