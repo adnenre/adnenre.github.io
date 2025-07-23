6b318b738a464bbaeadeb01a8aea63c2c60c407d
Adnen Rebai <adnen.rebai@gmail.com>
2025-07-23 20:13:23 +0200

    feat : add switch component

--- src/components/navigation/LanguageSwitcher.tsx
+++ src/components/navigation/LanguageSwitcher.tsx
@@ -1,12 +1,49 @@
 import { useLanguage } from "@/contexts/LanguageContext";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Languages } from "lucide-react";
+
+type Language = "en" | "fr";
+const languages: Record<Language, { name: string; flag: string }> = {
+  en: { name: "English", flag: "US" },
+  fr: { name: "French", flag: "FR" },
+};
 const LanguageSwitcher = () => {
-  const { language, toggleLanguage } = useLanguage();
+  const { language, setLanguage } = useLanguage();
 
   return (
-    <Button variant="default" onClick={toggleLanguage}>
-      {language.toUpperCase()}
-    </Button>
+    // <Button variant="default" onClick={toggleLanguage} className="w-5 h-6 mt-2">
+    //   {language.charAt(0).toUpperCase() + language.slice(1)}
+    // </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" className="h-8 w-8 border-0">
+          <Languages className="h-5 w-5 " />
+          <span className="sr-only">Switch language</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start">
+        {Object.entries(languages).map(([locale, { name, flag }]) => (
+          <DropdownMenuItem
+            key={locale}
+            onClick={() => setLanguage(locale)}
+            className={"cursor-pointer"}
+          >
+            <img
+              width="20px"
+              height="20px"
+              src={`https://flagsapi.com/${flag}/flat/64.png`}
+            />
+            <span className="mr-2">{name}</span>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
