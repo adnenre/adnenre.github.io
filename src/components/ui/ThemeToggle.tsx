8f4c9703579c66bb8855822a90eade467e18a994
Adnen Rebai <adnen.rebai@gmail.com>
2025-07-23 20:14:11 +0200

    fix : refactor themeToggle

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,5 +1,5 @@
 // components/ThemeToggle.tsx
-import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
 import { Sun, Moon } from "lucide-react";
 import { useTheme } from "@/contexts/ThemeContext";
 
@@ -7,17 +7,15 @@ export const ThemeToggle = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={toggleTheme}
-      aria-label="Toggle theme"
-    >
-      {theme === "light" ? (
-        <Moon className="h-4 w-4" />
-      ) : (
-        <Sun className="h-4 w-4" />
-      )}
-    </Button>
+    <div className="flex items-center space-x-2">
+      <Sun className="h-4 w-4 hover:cursor-pointer" onClick={toggleTheme} />
+      <Switch
+        checked={theme === "dark"}
+        onCheckedChange={toggleTheme}
+        aria-label="Toggle theme"
+        className="data-[state=checked]:bg-primary "
+      />
+      <Moon className="h-4 w-4 hover:cursor-pointer" onClick={toggleTheme} />
+    </div>
   );
 };
