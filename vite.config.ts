342424b88eef2b48664d79d19a55af15ac9dda40
Adnen Rebai <adnen.rebai@gmail.com>
2025-07-23 17:26:17 +0200

    fix : vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    headers: {
+      "Content-Type": "text/javascript",
+    },
+  },
   base: "/",
   resolve: {
     alias: {
