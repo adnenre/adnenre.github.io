17a2fac436ef48fc3be2c600db1855d1553d8e85
Adnen Rebai <adnen.rebai@gmail.com>
2025-10-15 04:10:58 +0100

    fix: navigation full width

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -5,12 +5,12 @@ import { ThemeToggle } from "../ui/ThemeToggle";
 export function Navigation() {
   return (
     <motion.header
-      className="sticky  bottom-0 left-0 right-0 z-50  flex justify-center  w-full   "
+      className=" z-50 w-screen  top-0 left-0 right-0  backdrop-blur-sm bg-background/30 border-b border-gray-200/10  fixed"
       initial={{ y: -100 }}
       animate={{ y: 0 }}
       transition={{ duration: 0.6, ease: [0.25, 0.25, 0, 1] }}
     >
-      <div className="navigation w-full container flex justify-between items-center relative  h-16 px-4">
+      <div className="navigation w-screen flex justify-between items-center relative  h-16 px-4">
         {/* Desktop Navigation */}
         <LanguageSwitcher />
         <div className="w-[100px]"></div>
